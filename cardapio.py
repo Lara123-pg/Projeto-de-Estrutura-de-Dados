573e466154ae09a7246e78145aca65e02c02d370
lara123-pg <lamorimalvescavalcante@gmail.com>
2023-03-04 19:54:45 -0300

    Atualização das listas do cardápio

--- cardapio.py
+++ cardapio.py
@@ -2,14 +2,19 @@ print('''
 Seja bem-vindo a lanchonete, abaixo temos as opções do nosso cardápio
 
 1 - Batata frita
-2 - Sanduíche com bacon
-3 - Sanduíche com frango
-4 - Sanduíche com carne
-5 - Refrigerante
+2 - Sanduíche de bacon
+3 - Sanduíche de frango
+4 - Sanduíche de carne
+5 - Sanduíche de queijo
+6 - Refrigerante
+7 - Suco
+8 - Sorvete
+9 - Bolo
+10 - Torta
 ''')
       
-opcoesCardapio = [1, 2, 3, 4, 5]
-cardapio = ['Batata frita', 'Sanduíche com bacon', 'Sanduíche com frango', 'Sanduíche com carne', 'Refrigerante']
+opcoesCardapio = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+cardapio = ['Batata frita', 'Sanduíche de bacon', 'Sanduíche de frango', 'Sanduíche de carne', 'Sanduíche de queijo', 'Refrigerante', 'Suco','Sorvete', 'Bolo', 'Torta']
 
 def busca_pedido(opcoes, op):
     left = 0
@@ -33,15 +38,15 @@ def fazer_pedido(pedido):
     adicionarPedido = busca_pedido(opcoesCardapio, pedido)
 
     if (adicionarPedido == -1):
-        print('Não temos essa opção no cardápio, por favor selecione opções entre 1 a 5')
-        print('------------------------------------------------------------------------')
+        print('Não temos essa opção no cardápio, por favor selecione opções entre 1 e 10')
+        print('-------------------------------------------------------------------------')
 
         return
     
     print(cardapio[adicionarPedido])
-    print('------------------------')
+    print('-----------------------')
 
-    return 'Pedido feito'
+    return 'Pedido anotado'
 
 while True:
     pedido = int(input('Digite a opção para pedir ou -1 para finalizar: '))
@@ -52,3 +57,4 @@ while True:
     fazerPedido = fazer_pedido(pedido)
 
     print(fazerPedido)
+    print('')
