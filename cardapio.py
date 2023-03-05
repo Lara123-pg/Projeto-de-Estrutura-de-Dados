f1412a35d3ee3ff5e52fae92767af3e07e67e9e0
lara123-pg <lamorimalvescavalcante@gmail.com>
2023-03-05 20:23:11 -0300

    Atualização do menu

--- cardapio.py
+++ cardapio.py
@@ -1,20 +1,23 @@
-print('''
-Seja bem-vindo a lanchonete, abaixo temos as opções do nosso cardápio
+print('Seja bem-vindo a lanchonete, abaixo temos as opções do nosso cardápio')
 
-1 - Batata frita
-2 - Sanduíche de bacon
-3 - Sanduíche de frango
-4 - Sanduíche de carne
-5 - Sanduíche de queijo
-6 - Refrigerante
-7 - Suco
-8 - Sorvete
-9 - Bolo
-10 - Torta
-''')
-      
 opcoesCardapio = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 cardapio = ['Batata frita', 'Sanduíche de bacon', 'Sanduíche de frango', 'Sanduíche de carne', 'Sanduíche de queijo', 'Refrigerante', 'Suco','Sorvete', 'Bolo', 'Torta']
+
+def menu():
+    print('''
+    Cardápio
+
+    1 - Batata frita
+    2 - Sanduíche de bacon
+    3 - Sanduíche de frango
+    4 - Sanduíche de carne
+    5 - Sanduíche de queijo
+    6 - Refrigerante
+    7 - Suco
+    8 - Sorvete
+    9 - Bolo
+    10 - Torta
+    ''')
 
 def busca_pedido(opcoes, op):
     left = 0
@@ -49,9 +52,13 @@ def fazer_pedido(pedido):
     return 'Pedido anotado'
 
 while True:
+    menu()
+
     pedido = int(input('Digite a opção para pedir ou -1 para finalizar: '))
 
     if (pedido == -1):
+        print('Pedido finalizado')
+
         break
     
     fazerPedido = fazer_pedido(pedido)
